4fc230276d0225a88a3654a67fcefb024413a5e0
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-10-20 21:17:51 +0530

    Update List type of extensions var

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class Parser:
     # Create an empty dictionary. Annotate the variable with the type of
     # List(str)
-    extensions: List(str) = []
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
         # To validate if a file have a parser, check if the extension part of
