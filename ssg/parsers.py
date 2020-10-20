61a3594564586a45723ff77dd2deb5487da4af5d
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-10-20 21:14:24 +0530

    Define extensions variable and create valid_ext fn

---
+++ ssg/parsers.py
@@ -0,0 +1,13 @@
+from typing import List
+from pathlib import Path
+
+
+class Parser:
+    # Create an empty dictionary. Annotate the variable with the type of
+    # List(str)
+    extensions: List(str) = []
+
+    def valid_extension(self, extension):
+        # To validate if a file have a parser, check if the extension part of
+        # the class extensions variable.
+        return extension in self.extensions
